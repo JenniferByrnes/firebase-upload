f3607ae432f36920b7ae17899f0a1fb8c36ccc99
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-11-19 18:33:43 -0500

    added percent counter and comments

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import { ref, uploadBytes, getDownloadURL, listAll } from "firebase/storage";
 import { v4 } from "uuid";
 
 function App() {
-  // the name of the image that is being uploaded.
+  // State for the uploaded image
   const [imageUpload, setImageUpload] = useState(null);
   const [imageUrls, setImageUrls] = useState([]);
 
-  // Get image refs to to a display of all images
-  const imagesListRef = ref(storage, "blogImages/");
+  // progress
+  const [percent, setPercent] = useState(0);
 
-  // Don't upload unless there is a file
+
+
+  // Handle file upload event and update state
   // blogImages is the folder where the image will be stored.
   const uploadFile = () => {
     if (!imageUpload) return;
@@ -23,12 +25,29 @@ function App() {
     uploadBytes(imageRef, imageUpload).then((snapshot) => {
       getDownloadURL(snapshot.ref).then((url) => {
         setImageUrls((prev) => [...prev, url]);
+
       });
-    });
+      const percent = Math.round(
+        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      );
+
+      // update progress
+      setPercent(percent);
+    },
+      (err) => console.log(err),
+      () => {
+        // download url
+        getDownloadURL(uploadBytes.snapshot.ref).then((url) => {
+          console.log(url);
+        });
+      });
   };
 
   // this is run at the opening of the page to display all images
+  // There is an error - this runs twice and doubles the images
   useEffect(() => {
+    // Get image refs to to a display of all images
+    const imagesListRef = ref(storage, "blogImages/");
     listAll(imagesListRef).then((response) => {
       response.items.forEach((item) => {
         getDownloadURL(item).then((url) => {
@@ -48,8 +67,9 @@ function App() {
         }}
       />
       <button onClick={uploadFile}> Upload Image</button>
+      <p>{percent} "% done"</p>
       {imageUrls.map((url) => {
-        return <img src={url} />;
+        return <img src={url} alt='' />;
       })}
     </div>
   );
